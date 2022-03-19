584e9c7f8fa72bce5fad1bf96205120b3b56fb15
habibmirsyad <habmirs@gmail.com>
2022-03-19 13:13:31 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPL_1302201138_MOD4_JURNAL_GGR
+{
+    public class Penjumlahan<T>
+    {
+        public void JumlahTigaAngka(T satu, T dua, T tiga)
+        {
+            int Satu = Convert.ToInt32(satu);
+            int Dua = Convert.ToInt32(dua);
+            int Tiga = Convert.ToInt32(tiga);
+            Console.WriteLine(Satu+Dua+Tiga);
+        }
+    }
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // nim = "1302201138";
+            Penjumlahan<int> penjumlahan = new Penjumlahan<int>();
+
+
+            penjumlahan.JumlahTigaAngka(13,02,20);
+        }
+    }
+}
